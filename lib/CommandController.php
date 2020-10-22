39f71cdd53d9919a3c1bf82776036997af3954dc
Daniel <mabadejedaniel1@gmail.com>
2020-10-22 04:35:22 -0700

    added command call class

--- lib/CommandController.php
+++ lib/CommandController.php
@@ -2,19 +2,51 @@
 
 namespace Minicli;
 
-abstract class CommandController
+class CommandNamespace
 {
-    protected $app;
+    protected $name;
 
-    abstract public function run($argv);
+    protected $controllers = [];
 
-    public function __construct(App $app)
+    public function __construct($name)
     {
-        $this->app = $app;
+        $this->name = $name;
     }
 
-    protected function getApp()
+    public function getName()
     {
-        return $this->app;
+        return $this->name;
+    }
+
+    public function loadControllers($commands_path)
+    {
+        foreach (glob($commands_path . '/' . $this->getName() . '/*Controller.php') as $controller_file) {
+            $this->loadCommandMap($controller_file);
+        }
+
+        return $this->getControllers();
+    }
+
+    public function getControllers()
+    {
+        return $this->controllers;
+    }
+
+    public function getController($command_name)
+    {
+        return isset($this->controllers[$command_name]) ? $this->controllers[$command_name] : null;
+    }
+
+    protected function loadCommandMap($controller_file)
+    {
+        $filename = basename($controller_file);
+
+        $controller_class = str_replace('.php', '', $filename);
+        $command_name = strtolower(str_replace('Controller', '', $controller_class));
+        $full_class_name = sprintf("App\\Command\\%s\\%s", $this->getName(), $controller_class);
+
+        /** @var CommandController $controller */
+        $controller = new $full_class_name();
+        $this->controllers[$command_name] = $controller;
     }
 }
